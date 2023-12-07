3747852e86b4a33992c72294692c4429d690e78f
Denys Pavlyuk <densf2@gmail.com>
2023-12-07 19:54:46 +0200

    configured_base_chages_for_login_steps

--- api_requests/usage_axios.js
+++ api_requests/usage_axios.js
@@ -1,16 +1,17 @@
 // import axios from 'axios'
 const axios = require('axios')
 const {expect} = require('chai')
+const data = require('./data/dummy_data.json')
+const fs = require('fs-extra')
 
-describe('Actions for dummy website', async() => {
-    let baseUrl = 'https://dummyjson.com';
+describe('Actions for dummy website', async() => {;
     let userId;
     let userName;
     let userPwd;
     let token;
 
     it.skip('Create user', async() => {
-        const createUser = await axios.post('https://dummyjson.com/users/add',
+        const createUser = await axios.post(`${data.baseUrl}/users/add`,
         {
             'firstName': 'Muhammad',
             'lastName': 'Ovi',
@@ -25,38 +26,15 @@ describe('Actions for dummy website', async() => {
         userId = createUser.data.id
     })
 
-    it('get user by id', async() =>{
-        const getUser = await axios.get('https://dummyjson.com/users/1')
-        // console.log(getUser.data)
-        userName = getUser.data.username
-        userPwd = getUser.data.password
-    })
-
-    it('getting credentials', async() => {
-        const getTokenData = await axios.post(`${baseUrl}/auth/login`,
-        {
-            'username': userName,
-            'password': userPwd,
-            expiresInMins: 30
-        },
-            {
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-        })
-        // console.log(getTokenData.data)
-        token = getTokenData.data.token
-    })
-
     it('create product', async() => {
-        const createProduct = await axios.post(`${baseUrl}/products/add`,
+        const createProduct = await axios.post(`${data.baseUrl}/products/add`,
         {
             'title': 'MyOwnProduct'
         },
             {
                 headers: {
                     'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${token}`
+                    'Authorization': `Bearer ${data.token}`
                 }
         })
         console.log(createProduct.data)
@@ -65,7 +43,7 @@ describe('Actions for dummy website', async() => {
 
 // updating lastName of user with id               
   it.skip('update user data', async () => {
-    const updateUserData = await axios.patch('https://dummyjson.com/users/101',
+    const updateUserData = await axios.patch(`${data.baseUrl}/users/101`,
     {
         'username': 'emailTut',
         'password': 'abc123',
